b0bc87bbb78bf752ed557eb3681353054f88bca8
Linus Rode <101805055+Asacus@users.noreply.github.com>
2024-08-03 17:27:42 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,108 +1,14 @@
-/* TEMPLATE GENERATED TESTCASE FILE
-Filename: CWE122_Heap_Based_Buffer_Overflow__c_src_wchar_t_cpy_33.cpp
-Label Definition File: CWE122_Heap_Based_Buffer_Overflow__c_src.label.xml
-Template File: sources-sink-33.tmpl.cpp
-*/
-/*
- * @description
- * CWE: 122 Heap Based Buffer Overflow
- * BadSource:  Initialize data as a large string
- * GoodSource: Initialize data as a small string
- * Sinks: cpy
- *    BadSink : Copy data to string using wcscpy
- * Flow Variant: 33 Data flow: use of a C++ reference to data within the same function
- *
- * */
+#include <iostream>
+#include <limits> // for std::numeric_limits
 
-#include "std_testcase.h"
-
-#include <wchar.h>
-
-namespace CWE122_Heap_Based_Buffer_Overflow__c_src_wchar_t_cpy_33
-{
-
-#ifndef OMITBAD
-
-void bad()
-{
-    wchar_t * data;
-    wchar_t * &dataRef = data;
-    data = (wchar_t *)malloc(100*sizeof(wchar_t));
-    if (data == NULL) {exit(-1);}
-    /* FLAW: Initialize data as a large buffer that is larger than the small buffer used in the sink */
-    wmemset(data, L'A', 100-1); /* fill with L'A's */
-    data[100-1] = L'\0'; /* null terminate */
-    {
-        wchar_t * data = dataRef;
-        {
-            wchar_t dest[50] = L"";
-            /* POTENTIAL FLAW: Possible buffer overflow if data is larger than dest */
-            wcscpy(dest, data);
-            printWLine(data);
-            free(data);
-        }
-    }
+void vulnerableFunction() {
+    int largeNumber = std::numeric_limits<int>::max() / 2 + 1; // A large number close to the max value of int
+    int result = largeNumber * 2; // This will cause an overflow
+    std::cout << "Vulnerable Result: " << result << std::endl; // This will not show the correct value due to overflow
 }
 
-#endif /* OMITBAD */
-
-#ifndef OMITGOOD
-
-/* goodG2B() uses the GoodSource with the BadSink */
-static void goodG2B()
-{
-    wchar_t * data;
-    wchar_t * &dataRef = data;
-    data = (wchar_t *)malloc(100*sizeof(wchar_t));
-    if (data == NULL) {exit(-1);}
-    /* FIX: Initialize data as a small buffer that as small or smaller than the small buffer used in the sink */
-    wmemset(data, L'A', 50-1); /* fill with L'A's */
-    data[50-1] = L'\0'; /* null terminate */
-    {
-        wchar_t * data = dataRef;
-        {
-            wchar_t dest[50] = L"";
-            /* POTENTIAL FLAW: Possible buffer overflow if data is larger than dest */
-            wcscpy(dest, data);
-            printWLine(data);
-            free(data);
-        }
-    }
-}
-
-void good()
-{
-    goodG2B();
-}
-
-#endif /* OMITGOOD */
-
-} /* close namespace */
-
-/* Below is the main(). It is only used when building this testcase on
- * its own for testing or for building a binary to use in testing binary
- * analysis tools. It is not used when compiling all the testcases as one
- * application, which is how source code analysis tools are tested.
- */
-#ifdef INCLUDEMAIN
-
-using namespace CWE122_Heap_Based_Buffer_Overflow__c_src_wchar_t_cpy_33; /* so that we can use good and bad easily */
-
-int main(int argc, char * argv[])
-{
-    /* seed randomness */
-    srand( (unsigned)time(NULL) );
-#ifndef OMITGOOD
-    printLine("Calling good()...");
-    good();
-    printLine("Finished good()");
-#endif /* OMITGOOD */
-#ifndef OMITBAD
-    printLine("Calling bad()...");
-    bad();
-    printLine("Finished bad()");
-#endif /* OMITBAD */
+int main() {
+    std::cout << "Demonstrating CWE-190 Integer Overflow" << std::endl;
+    vulnerableFunction();
     return 0;
 }
-
-#endif
